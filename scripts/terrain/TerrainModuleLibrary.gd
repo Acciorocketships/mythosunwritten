ecbac59eefb0fb664f6f97a92798ec55f0764c3d
Ryan Kortvelesy <acciorocketships@gmail.com>
2026-06-23 01:00:40 -0400

    refactor(terrain): delete unused 24x24x4 test piece (SP-1)

--- scripts/terrain/TerrainModuleLibrary.gd
+++ scripts/terrain/TerrainModuleLibrary.gd
@@ -37,7 +37,6 @@ func load_test_pieces() -> void:
 	terrain_modules.append(TerrainModuleDefinitions.create_8x8_test_piece())
 	terrain_modules.append(TerrainModuleDefinitions.create_12x12_test_piece())
 	terrain_modules.append(TerrainModuleDefinitions.create_24x24_test_piece())
-	terrain_modules.append(TerrainModuleDefinitions.create_24x24x4_test_piece())
 	terrain_modules.append(TerrainModuleDefinitions.create_4x4x4_test_piece())
 
 
